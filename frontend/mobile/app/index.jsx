53a5108d42933c7cec1a354fc1c367e4968b793a
Muhammad Azim <azeemamjad412@gmail.com>
2024-05-12 17:33:08 +0500

    Setup TailwindMerger and Tailwind using NativeWind

--- frontend/mobile/app/index.jsx
+++ frontend/mobile/app/index.jsx
@@ -1,59 +1,19 @@
-import { StyleSheet, Text, View, ImageBackground, Pressable, TouchableOpacity } from 'react-native'
+import { Text, View, ImageBackground, Pressable } from 'react-native'
+import Button from "../components/Button"
 import React from 'react'
 
 
 const Welcome = () => {
   return (
-    <View style={styles.container}>
-      <ImageBackground source={require('../assets/images/welcome.jpg')} resizeMode="cover" style={styles.image}>
-        <Text style= {{
-           color: '#FFF',
-           fontFamily: 'Poppins',
-           fontSize: 30,
-           fontStyle: 'normal',
-           fontWeight: '700',
-           lineHeight: 39, // Since 130% of 30px is approximately 39px
-           letterSpacing: -0.3,
-           marginBottom: 40
-        }}>Doctor Assistance</Text>
-        <TouchableOpacity style={{
-          justifyContent: 'center',
-          alignItems: 'center',
-          width: 330,
-          height: 56,
-          padding: 10,
-          borderRadius: 10,
-          backgroundColor: "#045883"
-        }} onPress={() => console.log('Pressed')}>
-          <Text style={{
-            color: '#FFF',
-            fontFamily: 'Poppins',
-            fontSize: 14,
-            fontStyle: 'normal',
-            textAlign: 'center',
-            // backgroundColor: '#000000c0',
-          }}>Sign In</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={{
-          justifyContent: 'center',
-          alignItems: 'center',
-          width: 330,
-          height: 56,
-          padding: 10,
-          borderColor: '#ffff',
-          borderWidth: 1,
-          borderRadius: 10,
-          marginHorizontal: 90
-        }} onPress={() => console.log('Pressed')}>
-          <Text style={{
-            color: '#FFF',
-            fontFamily: 'Poppins',
-            fontSize: 14,
-            fontStyle: 'normal',
-            textAlign: 'center',
-            // backgroundColor: '#000000c0',
-          }}>Create Account</Text>
-        </TouchableOpacity>
+    <View className="flex-1">
+      <ImageBackground source={require('../assets/images/welcome.jpg')} resizeMode="cover" className="justify-end flex-1 items-center gap-3 p-5">
+        <Text className="text-white text-2xl font-bold mb-24">Doctor Assistance</Text>
+        <Button className="bg-[#045883]">
+          <Text className="color-white text-base">Sign In</Text>
+        </Button>
+        <Button className="border border-white" onPress={() => console.log('Pressed')}>
+          <Text className="color-white text-base">Create Account</Text>
+        </Button>
       </ImageBackground>
     </View>
   )
@@ -61,35 +21,3 @@ const Welcome = () => {
 
 export default Welcome
 
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  image: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    padding: 20,
-    gap: 10
-  },
-  text: {
-    color: 'white',
-    fontSize: 42,
-    lineHeight: 84,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    // backgroundColor: '#000000c0',
-  },
-  pressable: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: 353,
-    height: 56,
-    padding: 10,
-    borderColor: '#ffff',
-    borderWidth: 1,
-    borderRadius: 10,
-    backgroundColor: "#045883"
-  }
-});
---
+++ frontend/mobile/components/Button.jsx
@@ -0,0 +1,13 @@
+import {Pressable} from 'react-native'
+import React from 'react'
+import { twMerge } from 'tailwind-merge'
+
+const Button = ({children, className, ...props}) => {
+  return (
+    <Pressable className={twMerge("justify-center items-center w-[330px] h-14 rounded-lg p-3 hover:bg-opacity-65", className)} {...props}>
+        {children}
+    </Pressable>
+  )
+}
+
+export default Button
