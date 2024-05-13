30a1b40f87abd1fec7c7a5d4f82b1f0149492e29
Muhammad Sabir <sabir.tabi1122@gmail.com>
2024-05-13 21:12:30 +0500

    Add Authentication and related models

--- backend/doctor_assitance/core/models.py
+++ backend/doctor_assitance/core/models.py
@@ -3,22 +3,30 @@ Database Models
 """
 
 from django.db import models
-from django.core.validators import RegexValidator
+from django.conf import settings
+from django.core.validators import RegexValidator, MinValueValidator, MaxValueValidator
 from django.contrib.auth.models import (
-    AbstractBaseUser, BaseUserManager, PermissionsMixin
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
 )
 
 
 class UserManager(BaseUserManager):
-    """ Manager for user model """
+    """Manager for user model"""
 
-    def create_user(self, phone, password=None, **extra_fields):
+    def create_user(self, phone, is_doctor, password=None, **extra_fields):
         if not phone:
-            raise ValueError('Phone number is required.')
+            raise ValueError("Phone number is required.")
 
-        user = self.model(phone=phone, **extra_fields)
+        user = self.model(phone=phone, is_doctor=is_doctor, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
+
+        if is_doctor:
+            Doctor.objects.create(user=user)
+        else:
+            Patient.objects.create(user=user)
 
         return user
 
@@ -30,44 +38,76 @@ class UserManager(BaseUserManager):
 
         return user
 
-class User(AbstractBaseUser, PermissionsMixin):
-    """ User in the system. """
 
-    phone_regex = RegexValidator(regex=r'^\+?\d{9,19}$',
-        message="Phone number must be entered in the format: '999999999'. Up to 15 digits allowed.")
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+
+    phone_regex = RegexValidator(
+        regex=r"^\+?\d{9,19}$",
+        message="Phone number must be entered in the format: '999999999'. Up to 15 digits allowed.",
+    )
 
     phone = models.CharField(max_length=20, unique=True, validators=[phone_regex])
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    is_doctor = models.BooleanField(default=False)
-    is_authenticated = models.BooleanField(default=False)
+    is_doctor = models.BooleanField(default=False, null=False)
     is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
     """ The field we want to use for authentication """
-    USERNAME_FIELD = 'phone'
+    USERNAME_FIELD = "phone"
 
 
-# Create your models here.
-# class User(models.Model):
-#     ACCOUNT_TYPES = [
-#         ('doctor', 'doctor'),
-#         ('patient', 'patient'),
-#     ]
+class Doctor(models.Model):
+    """Doctor model."""
 
-#     name = models.CharField(max_length=255)
-#     email = models.EmailField(max_length=255, unique=True)
-#     phone = models.CharField(max_length=11, unique=True)
-#     password = models.CharField(max_length=255)
-#     account_type = models.CharField(max_length=7, choices=ACCOUNT_TYPES, default='patient')
-#     is_authenticated = models.BooleanField(default=False)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    experience = models.IntegerField(
+        default=0, validators=[MinValueValidator(0), MaxValueValidator(99)]
+    )
+    date_of_birth = models.DateField(
+        null=True,
+    )
 
-# class User(models.Model):
-#     user_id = models.AutoField(primary_key=True)
-#     name = models.CharField(max_length=100)
-#     email = models.EmailField(max_length=100)
-#     phone = models.CharField(max_length=20)
-#     password = models.CharField(max_length=100)
-#     is_authenticated = models.BooleanField(default=False)
-#     account_type = models.CharField(max_length=20)
+
+class Degree(models.Model):
+    """Degree model."""
+
+    degree_name = models.CharField(max_length=100)
+    doctors = models.ManyToManyField("Doctor", through="DoctorDegree")
+
+
+class Specialization(models.Model):
+    """Specialization model."""
+
+    specialization_name = models.CharField(max_length=100)
+    doctors = models.ManyToManyField("Doctor", through="DoctorSpecialization")
+
+
+class DoctorDegree(models.Model):
+    """Junction table between Doctor and Degree."""
+
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    degree = models.ForeignKey(Degree, on_delete=models.CASCADE)
+
+
+class DoctorSpecialization(models.Model):
+    """Junction table between Doctor and Specialization."""
+
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    specialization = models.ForeignKey(Specialization, on_delete=models.CASCADE)
+
+
+class Patient(models.Model):
+    """Patient model."""
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
--- backend/doctor_assitance/core/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import Login
-
-urlpatterns = [
-    path('login/', Login.as_view()), 
-]
